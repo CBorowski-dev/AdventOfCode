446f83e55c5210d6abe5a71199cfa3474d7e06b3
CBorowski-dev <christoph.borowski@gmx.de>
2024-12-17 07:50:29 +0100

    File update

--- 2024/src/de/borowski/aoc/Day16_ReindeerMaze.java
+++ 2024/src/de/borowski/aoc/Day16_ReindeerMaze.java
@@ -11,10 +11,9 @@ public class Day16_ReindeerMaze {
     String filename = "/home/christoph/Projects/IdeaProjects/AdventOfCode/2024/input/input_day16.txt";
 
     private final char[][] maze = new char[size][size];
-    private final char[][] visited = new char[size][size];
     private int rX = 0;
     private int rY = 0;
-    private int bestScore = -1;
+    private int bestScore = 87380;
 
     enum Direction {
         NORTH, EAST, SOUTH, WEST;
@@ -22,7 +21,7 @@ public class Day16_ReindeerMaze {
 
     public Day16_ReindeerMaze() {
         readInput();
-        initVisited();
+        // initVisited();
         moveReindeer(rY, rX, Direction.EAST, 0);
         System.out.println("--------------------------------");
         System.out.println(bestScore);
@@ -30,47 +29,40 @@ public class Day16_ReindeerMaze {
 
     private void moveReindeer(int y, int x, Direction d, int score) {
         if (maze[y][x]=='E') {
-            printMaze(score);
+            System.out.println("---------------" + score + "---------------------");
+            // printMaze(score);
             if (bestScore == -1 || bestScore > score) bestScore = score;
         }
-        //int currentScore = -1;
+        if (score + (size-2-x + y-1) + ((x!=size-2 && y>1)?1000:0)> bestScore) return;
         char c = maze[y][x];
         maze[y][x] = 'x';
-        // NORTH
-        if (!d.equals(Direction.SOUTH) && maze[y-1][x] != '#' && maze[y-1][x] != 'x') {
-            moveReindeer(y-1, x, Direction.NORTH, score + ((d.equals(Direction.NORTH)) ? 1 : 1001));
-            //if (currentScore == -1 || currentScore > score) currentScore = score;
-        }
+
         // EAST
-        if (!d.equals(Direction.WEST) && maze[y][x+1] != '#' && maze[y][x+1] != 'x') {
+        if (!d.equals(Direction.WEST) && maze[y][x+1] != '#' && maze[y][x+1] != 'x')
             moveReindeer(y, x+1, Direction.EAST, score + ((d.equals(Direction.EAST)) ? 1 : 1001));
-            //if (currentScore == -1 || currentScore > score) currentScore = score;
-        }
-        // SOUTH
-        if (!d.equals(Direction.NORTH) && maze[y+1][x] != '#' && maze[y+1][x] != 'x') {
-            moveReindeer(y+1, x, Direction.SOUTH, score + ((d.equals(Direction.SOUTH)) ? 1 : 1001));
-            // if (currentScore == -1 || currentScore > score) currentScore = score;
-        }
+        // NORTH
+        if (!d.equals(Direction.SOUTH) && maze[y-1][x] != '#' && maze[y-1][x] != 'x')
+            moveReindeer(y-1, x, Direction.NORTH, score + ((d.equals(Direction.NORTH)) ? 1 : 1001));
         // WEST
-        if (!d.equals(Direction.EAST) && maze[y][x-1] != '#' && maze[y][x-1] != 'x') {
+        if (!d.equals(Direction.EAST) && maze[y][x-1] != '#' && maze[y][x-1] != 'x')
             moveReindeer(y, x-1, Direction.WEST, score + ((d.equals(Direction.WEST)) ? 1 : 1001));
-            //if (currentScore == -1 || currentScore > score) currentScore = score;
-        }
+        // SOUTH
+        if (!d.equals(Direction.NORTH) && maze[y+1][x] != '#' && maze[y+1][x] != 'x')
+            moveReindeer(y+1, x, Direction.SOUTH, score + ((d.equals(Direction.SOUTH)) ? 1 : 1001));
+
         maze[y][x] = c;
-        // return currentScore;
     }
 
-    private void initVisited() {
+    /* private void initVisited() {
         for (int y = 0; y< maze.length; y++) {
             for (int x = 0; x< maze.length; x++) {
                 visited[y][x] = maze[y][x];
                 if (visited[y][x]=='E' || visited[y][x]=='S') visited[y][x]='.';
             }
         }
-    }
+    }*/
 
     private void printMaze(int score) {
-        System.out.println("---------------" + score + "---------------------");
         for (int y = 0; y< maze.length; y++) {
             for (int x = 0; x< maze.length; x++) {
                 System.out.print(maze[y][x]);
@@ -104,4 +96,5 @@ public class Day16_ReindeerMaze {
     }
 
     public static void main(String[] args) { new Day16_ReindeerMaze(); }
+    // 87380 too high
 }
