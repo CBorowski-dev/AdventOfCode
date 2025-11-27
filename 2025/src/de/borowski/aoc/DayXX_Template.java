51485458e64a1b4212aa3aad7d5e16b20f398d90
Christoph Borowski <borowski.christoph@gmail.com>
2025-11-27 15:12:31 +0100

    Added project for 2025 AoC

---
+++ 2025/src/de/borowski/aoc/DayXX_Template.java
@@ -0,0 +1,34 @@
+package de.borowski.aoc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class DayXX_Template {
+
+	public static void main(String[] args) {
+		BufferedReader reader;
+		
+		try {
+			// reader = new BufferedReader(new FileReader("/home/christoph/Projects/IdeaProjects/AdventOfCode/2025/input/input_day01_testset.txt"));
+			reader = new BufferedReader(new FileReader("/home/christoph/Projects/IdeaProjects/AdventOfCode/2025/input/input_day01.txt"));
+			String line = reader.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				// read next line
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("--------------------------------");
+	}
+
+}
