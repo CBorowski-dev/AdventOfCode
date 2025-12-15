99155e34a8fcd3573344eb57924bc16a4426ab3d
CBorowski-dev <christoph.borowski@gmx.de>
2025-12-15 16:32:55 +0100

    Solution for day 11

--- 2025/src/de/borowski/aoc/Day11_Reactor.java
+++ 2025/src/de/borowski/aoc/Day11_Reactor.java
@@ -7,68 +7,88 @@ import java.util.*;
 
 public class Day11_Reactor {
 
-	public static final Map<String, List<String>> CON = new HashMap<>();
-	public static int counter = 0;
+    public static final Map<String, Node> CON = new HashMap<>();
 
+    public static void main(String[] args) {
+        BufferedReader reader;
 
-	public static void main(String[] args) {
-		BufferedReader reader;
+        try {
+            // reader = new BufferedReader(new FileReader("/home/christoph/Projects/IdeaProjects/AdventOfCode/2025/input/input_day11_testset.txt"));
+            reader = new BufferedReader(new FileReader("/home/christoph/Projects/IdeaProjects/AdventOfCode/2025/input/input_day11.txt"));
+            String line = reader.readLine();
 
-		try {
-			// reader = new BufferedReader(new FileReader("/home/christoph/Projects/IdeaProjects/AdventOfCode/2025/input/input_day11_testset.txt"));
-			reader = new BufferedReader(new FileReader("/home/christoph/Projects/IdeaProjects/AdventOfCode/2025/input/input_day11.txt"));
-			String line = reader.readLine();
+            while (line != null) {
+                // System.out.println(line);
+                String[] nodes = line.split(": ");
+                String[] toNodes = nodes[1].split(" ");
+                Node key = CON.get(nodes[0]);
+                if (key == null) {
+                    key = new Node(nodes[0], generateChildNodeList(toNodes));
+                    CON.put(nodes[0], key);
+                } else if (key.childNodes == null) {
+                    key.childNodes = generateChildNodeList(toNodes);
+                }
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-			while (line != null) {
-				// System.out.println(line);
-				String[] nodes = line.split(": ");
-				String[] toNodes = nodes[1].split(" ");
-				CON.put(nodes[0], new ArrayList<String>(Arrays.stream(toNodes).toList()));
-				// read next line
-				line = reader.readLine();
-			}
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        // Part 1
+        Node node = CON.get("you");
+        searchAllPaths(node, "out");
 
-		// Part 1
-		// searchAllPaths(CON.get("you"));
-		// Part 2
-		searchAllSpecialPaths(CON.get("svr"));
-		System.out.println("--------------------------------");
-		System.out.println(counter);
+        // Part 2: Run part 1 for the following combination and multiply the tree numbers for the result
+        // srv -> fft : 8049
+        // fft -> dac : 3377314
+        // dac -> out : 12215
+        System.out.println("--------------------------------");
+        System.out.println(node.counter);
+    }
 
-	}
+    private static List<Node> generateChildNodeList(String[] toNodes) {
+        List<Node> childNodes = new ArrayList<>();
+        for (String s : toNodes) {
+            Node key = CON.get(s);
+            if (key == null) {
+                key = new Node(s);
+                CON.put(s, key);
+            }
+            childNodes.add(key);
+        }
+        return childNodes;
+    }
 
-	private static void searchAllPaths(List<String> nodes) {
-		for (String n: nodes) {
-			if (n.equals("out")) counter++;
-			else searchAllPaths(CON.get(n));
-		}
-	}
+    private static void searchAllPaths(Node node, String target) {
+        for (Node childNode : node.childNodes) {
+            if (childNode.label.equals(target)) {
+                node.counter = 1;
+            } else if (!childNode.visited) {
+                if (childNode.childNodes != null) searchAllPaths(childNode, target);
+                node.counter += childNode.counter;
+            } else {
+                node.counter += childNode.counter;
+            }
+        }
+        node.visited = true;
+    }
 
-	private static void searchAllSpecialPaths(List<String> nodes) {
-		for (String n: nodes) {
-			if (n.equals("fft")) {
-				counter++;
-				System.out.print('*');
-			} else {
-				List<String> nodes2 = CON.get(n);
-				if (nodes2 != null) searchAllSpecialPaths(nodes2);
-			}
-		}
-	}
+    public static class Node {
+        String label;
+        long counter = 0;
+        List<Node> childNodes;
+        boolean visited = false;
 
-	public class Node {
-		String label;
-		int counter = 0;
-		List<String> childNodes;
+        public Node(String label) {
+            this.label = label;
+        }
 
-		public Node(String label, List<String> childNodes) {
-			this.label = label;
-			this.childNodes = childNodes;
-		}
-	}
+        public Node(String label, List<Node> childNodes) {
+            this.label = label;
+            this.childNodes = childNodes;
+        }
+    }
 
 }
